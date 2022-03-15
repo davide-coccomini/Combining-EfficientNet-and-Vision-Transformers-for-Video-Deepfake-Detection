d436e34217d325a28e1b96575eb63ef4ad5a1e80
Davide Coccomini <davidecoccomini@gmail.com>
2022-03-15 16:54:47 +0100

    Improve code for FaceForensics dataset face detection

--- preprocessing/utils.py
+++ preprocessing/utils.py
@@ -3,27 +3,31 @@ import os
 from glob import glob
 from pathlib import Path
 import cv2
+banned_folders = ["boxes", "set", "splits", "actors", "crops", "DeepFakeDetection", "actors", "zip"]
 def get_video_paths(data_path, dataset, excluded_videos=[]):
     videos_folders = os.listdir(data_path)
     videos_paths = []
     for folder in videos_folders:
-        if folder == "boxes" or "set" in folder or folder == "splits" or folder == "actors" or folder == "crops" or folder == "DeepFakeDetection" or "zip" in folder:
+        if any(banned_folder in folder for banned_folder in banned_folders):
             continue
         
-
         folder_path = os.path.join(data_path, folder)
-        
         if dataset == 1:
-            folder_path = os.path.join(folder_path, "c23", "videos")
-        
+            internal_folders = os.listdir(folder_path)
+            for internal_folder in internal_folders:
+                internal_path = os.path.join(folder_path, internal_folder)
+                internal_path = os.path.join(internal_path, "c23", "videos")
+                videos_paths.extend([os.path.join(internal_path, video_name) for video_name in os.listdir(internal_path)])
+            
+        else:
+            if not os.path.isdir(folder_path):
+                return videos_paths.extend([os.path.join(data_path, video_name) for video_name in videos_folders])
 
-        if not os.path.isdir(folder_path):
-            return [os.path.join(data_path, video_name) for video_name in videos_folders]
-
-        for index, video in enumerate(os.listdir(folder_path)):
-            if "metadata" in video or video.split(".")[0] in excluded_videos:
-                continue
-            videos_paths.append(os.path.join(folder_path, video))
+            for index, video in enumerate(os.listdir(folder_path)):
+                if "metadata" in video or video.split(".")[0] in excluded_videos:
+                    continue
+                videos_paths.append(os.path.join(folder_path, video))
+    
     return videos_paths
 
 def resize(image, image_size):
